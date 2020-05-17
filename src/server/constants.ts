c7b14f2ce1a75f66dc1a7179d8c82e47845ff1cd
Shubham Naik <shub@shub.club>
2020-05-16 21:18:40 -0700

    chore: add heroku mongo env

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -4,7 +4,10 @@ export const DATABASE_NAME =
     ENVIRONMENT === 'testing' ? 'qtip-testing' : 'qtip';
 
 export const DATABASE_URI =
-    process.env.MONGODB_URI || 'mongodb://localhost:27017';
+           // heroku
+           process.env.MONGO_URI ||
+           process.env.MONGODB_URI ||
+           'mongodb://localhost:27017';
 
 export const QTIP_AUTH_TOKEN = process.env.QTIP_AUTH_TOKEN;
 
